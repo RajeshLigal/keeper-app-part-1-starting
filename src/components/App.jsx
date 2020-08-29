76961373e96f0cbd42629f010e5a1e2ffd02474d
Rajesh Ligal <rajeshligal21@gmail.com>
2020-08-30 01:42:59 +0545

    Adds add Functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [inputText, setInputText] = useState({
+    title: "",
+    content: "",
+  });
+  const [postContent, setPostContent] = useState([]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setInputText((preValue) => {
+      return { ...preValue, [name]: value };
+    });
+  }
+
+  function handleClick(event) {
+    setPostContent((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+    event.preventDefault();
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea
+        title={inputText.title}
+        content={inputText.content}
+        inputChange={handleChange}
+        submit={handleClick}
+      />
+      {postContent.map((noteItem) => (
+        <Note key={1} title={noteItem.title} content={noteItem.content} />
+      ))}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={props.submit}>
+        <input
+          onChange={props.inputChange}
+          name="title"
+          placeholder="Title"
+          value={props.title}
+        />
+        <textarea
+          onChange={props.inputChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={props.content}
+        />
         <button>Add</button>
       </form>
     </div>
